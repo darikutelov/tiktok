2edf0f96e0a1cb3e3dc03694530798e01eaed877
darikutelov <dari@kutelov.com>
2024-02-17 17:27:16 +0200

    Initial files

---
+++ TikTokUIKit/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TikTokUIKit
+//
+//  Created by Dariy Kutelov on 17.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
